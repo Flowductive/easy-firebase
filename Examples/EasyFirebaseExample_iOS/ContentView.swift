5c6441717188ef34ab46d0943847b402971a13b3
Ben Myers <benl.myers01@gmail.com>
2022-02-06 15:34:57 -0800

    🎓 Add Example project for iOS

---
+++ Examples/EasyFirebaseExample_iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EasyFirebaseExample_iOS
+//
+//  Created by Ben Myers on 2/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  
+  // MARK: - Body View
+  
+  var body: some View {
+    Text("Hello, world!")
+      .padding()
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
---
+++ Examples/EasyFirebaseExample_iOS/EasyFirebaseExample_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyFirebaseExample_iOSApp.swift
+//  EasyFirebaseExample_iOS
+//
+//  Created by Ben Myers on 2/4/22.
+//
+
+import SwiftUI
+
+@main
+struct EasyFirebaseExample_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
