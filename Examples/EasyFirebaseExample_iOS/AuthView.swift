a3982ed8bd8721a45a33d293d6e9333df4ae0fbd
Ben Myers <benl.myers01@gmail.com>
2022-02-08 02:10:37 -0800

    🤯 Add additional social previews

--- Examples/EasyFirebaseExample_iOS/AuthView.swift
+++ Examples/EasyFirebaseExample_iOS/AuthView.swift
@@ -6,19 +6,100 @@
 //
 
 import SwiftUI
+import EasyFirebase
 
 /**
  This view assists with learning `EasyAuth`.
  */
 struct AuthView: View {
   
+  // MARK: - Wrapped Properties
+  
+  /// The Global `EnvironmentObject`.
+  @EnvironmentObject var global: Global
+  
+  /// The field holding the email input.
+  @State var emailField: String = ""
+  /// The field holding the password input.
+  @State var passwordField: String = ""
+  /// The error that occured, if any.
+  @State var error: String? = nil
+  
   // MARK: - Body View
   
   var body: some View {
-    Text("AuthView")
+    VStack {
+      errorView
+      if let user = global.user, user.id != "Guest" {
+        infoView
+      } else {
+        fieldsView
+      }
+    }.padding()
   }
   
   // MARK: - Supporting Views
+  
+  /// A view displaying an error that occured, if any.
+  var errorView: some View {
+    Group {
+      if let error = error {
+        Text(error).foregroundColor(.red)
+      }
+    }
+  }
+  
+  /// A view that assists with account creation and signing in.
+  var fieldsView: some View {
+    VStack {
+      TextField("Email", text: $emailField)
+        .textContentType(.emailAddress)
+        .keyboardType(.emailAddress)
+      TextField("Password", text: $passwordField)
+        .textContentType(.password)
+      HStack {
+        Button(action: signIn) {
+          Text("Sign In")
+        }
+        Spacer().frame(width: 50.0)
+        Button(action: createAccount) {
+          Text("Create Account")
+        }
+      }
+    }
+  }
+  
+  /// Displays available information about the user.
+  var infoView: some View {
+    VStack {
+      Text("You are signed in.")
+        .fontWeight(.bold)
+      Text("Username: \(global.user.username)")
+      Text("Email: \(global.user.email)")
+      Text("Display Name: \(global.user.displayName)")
+      Text("Fav Food: \(global.user.favoriteFood)")
+      Text("Age: \(global.user.age)")
+      Text("Has Job: \(String(global.user.hasJob))")
+    }
+  }
+  
+  // MARK: - Methods
+  
+  func signIn() {
+    EasyAuth.signIn(email: emailField, password: passwordField) { error in
+      self.error = error?.localizedDescription
+    }
+  }
+  
+  func createAccount() {
+    EasyAuth.createAccount(email: emailField, password: passwordField) { error in
+      self.error = error?.localizedDescription
+    }
+  }
+  
+  func signOut() {
+    EasyAuth.signOut()
+  }
 }
 
 struct AuthView_Previews: PreviewProvider {
@@ -26,3 +107,5 @@ struct AuthView_Previews: PreviewProvider {
     AuthView()
   }
 }
+
+EasyAuth.signOut()
