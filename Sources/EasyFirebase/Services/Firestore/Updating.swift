38683942d0375f42312d829c1cd4b86c6b04921e
Ben Myers <benl.myers01@gmail.com>
2022-08-18 10:36:35 -0700

    🔗 Firestore array updating methods

--- Sources/EasyFirebase/Services/Firestore/Updating.swift
+++ Sources/EasyFirebase/Services/Firestore/Updating.swift
@@ -32,5 +32,55 @@ extension EasyFirestore {
       let collectionName = String(describing: T.self)
       db.collection(collectionName).document(document.id).updateData([path.string: FieldValue.increment(Int64(increase))], completion: completion)
     }
+    
+    /**
+     Appends a value to an array in a field in Firestore.
+     
+     - parameter path: The path to the document's field to update.
+     - parameter item: The new item to append.
+     - parameter document: The document to modify.
+     - parameter completion: The completion handler.
+     */
+    public static func append<T, U>(_ path: KeyPath<T, Array<U>>, with item: U, in document: T, completion: @escaping (Error?) -> Void = { _ in }) where T: Document {
+      append(path, with: [item], in: document, completion: completion)
+    }
+    
+    /**
+     Appends a value to an array in a field in Firestore.
+     
+     - parameter path: The path to the document's field to update.
+     - parameter items: The new items to append.
+     - parameter document: The document to modify.
+     - parameter completion: The completion handler.
+     */
+    public static func append<T, U>(_ path: KeyPath<T, Array<U>>, with items: Array<U>, in document: T, completion: @escaping (Error?) -> Void = { _ in }) where T: Document {
+      let collectionName = String(describing: T.self)
+      db.collection(collectionName).document(document.id).updateData([path.string: FieldValue.arrayUnion(items)], completion: completion)
+    }
+    
+    /**
+     Removes a value from an array in a field in Firestore.
+     
+     - parameter path: The path to the document's field to update.
+     - parameter item: The item to remove.
+     - parameter document: The document to modify.
+     - parameter completion: The completion handler.
+     */
+    public static func remove<T, U>(_ path: KeyPath<T, Array<U>>, taking item: U, in document: T, completion: @escaping (Error?) -> Void = { _ in }) where T: Document {
+      remove(path, taking: [item], in: document, completion: completion)
+    }
+    
+    /**
+     Appends a value to an array in a field in Firestore.
+     
+     - parameter path: The path to the document's field to update.
+     - parameter items: The items to remove.
+     - parameter document: The document to modify.
+     - parameter completion: The completion handler.
+     */
+    public static func remove<T, U>(_ path: KeyPath<T, Array<U>>, taking items: Array<U>, in document: T, completion: @escaping (Error?) -> Void = { _ in }) where T: Document {
+      let collectionName = String(describing: T.self)
+      db.collection(collectionName).document(document.id).updateData([path.string: FieldValue.arrayRemove(items)], completion: completion)
+    }
   }
 }
