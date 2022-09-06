b45c4cc96de8f4b7ae2dbe7a0e61728d7d0d91c7
Ben Myers <benl.myers01@gmail.com>
2022-09-06 11:46:29 -0700

    ☺️ Add new example project

---
+++ Examples/EasyFirebaseExample/Shared/EasyFirebaseExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyFirebaseExampleApp.swift
+//  Shared
+//
+//  Created by Ben Myers on 9/6/22.
+//
+
+import SwiftUI
+
+@main
+struct EasyFirebaseExampleApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
